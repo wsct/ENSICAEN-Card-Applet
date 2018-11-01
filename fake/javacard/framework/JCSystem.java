7317a0887b16c2d7d8c3c11efcd85b39a6775278
Tok <github@zetoken.net>
2018-11-01 15:25:35 +0100

    - fix: MF selection unit test broke because of the transient array

--- fake/javacard/framework/JCSystem.java
+++ fake/javacard/framework/JCSystem.java
@@ -5,6 +5,6 @@ public class JCSystem {
     public static final byte CLEAR_ON_RESET = 1;
 
     public static byte[] makeTransientByteArray(short length, byte event) {
-        return null;
+        return new byte[length];
     }
 }
